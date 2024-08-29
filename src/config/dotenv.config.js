f9ff39b7715205e0a18fa24045004b79442e9a17
scino11 <prod.scino@gmail.com>
2024-08-29 18:32:52 -0300

    db

--- src/config/dotenv.config.js
+++ src/config/dotenv.config.js
@@ -6,7 +6,7 @@ export const PORT = process.env.PORT || 3000
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || '7410056Chino!'
-export const DB_DATABASE= process.env.DB_DATABASE || 'tododb'
+export const DB_DATABASE= process.env.DB_DATABASE || 'todo db'
 export const DB_PORT = process.env.DB_PORT || 3306
 
 
