fd8b968f844c3f3ef47271823a332e560bbc74b8
scino11 <prod.scino@gmail.com>
2024-08-29 17:52:50 -0300

    pong

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import taskRouter from './routes/tasks.router.js'
 import userRouter from './routes/users.router.js'
+import { pool } from './config/database.js';
 
 const app = express();
 app.disable('x-powered-by')
 
+app.get('/ping', async (req, res) => {
+    const [result] = await pool.query('SELECT "Pong" AS result')
+    res.json(result[0])
+})
 app.use(express.json())
-app.use('/api',userRouter)
+app.use('/api', userRouter)
 app.use(taskRouter)
 
 app.use((req,res,next) => {
