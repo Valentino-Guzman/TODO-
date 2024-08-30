445bd0de180f10c4e1f83e82bbb76c7122fe6724
scino11 <prod.scino@gmail.com>
2024-08-30 14:00:16 -0300

    cors config

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -1,5 +1,5 @@
 export const corsOptions = {
-    origin: 'http://localhost:4200/', 
+    origin: 'http://localhost:4200', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],     
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true 
